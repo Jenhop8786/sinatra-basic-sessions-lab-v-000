7a4ae23cd2b02d27dad955c1bce20c61f4d3774d
Jenelynrose Hopkins <jenelyn.hopkins@gmail.com>
2018-03-14 20:17:47 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,20 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  configure do 
-    enable :sessions 
+  configure do
+    enable :sessions
     set :session_secret, "secret"
 end#do
 
-get '/' do 
+get '/' do
 
   erb :index
-end#get 
+end#get
 
-post '/checkout' do 
+post '/checkout' do
   session[item] = params[item]
-  @session = session 
+  @session = session
 
-  erb :checkout 
+  erb :checkout
 end#post 
 end#class
